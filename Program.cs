848713a595e956ca87cfb1dbc53af41893d70058
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-07-02 15:25:22 -0700

    WIP writing TopMenu

--- Program.cs
+++ Program.cs
@@ -6,9 +6,17 @@ namespace Bakery
 {
   public class Program
   {
+    public Bread bread;
+    public Pastry pastry;
     static void Main()
     {
-      
+
+    }
+
+    public void TopMenu()
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("Would you like to order some bread? Type Y for yes, or N for no.");
     }
   }
 }
