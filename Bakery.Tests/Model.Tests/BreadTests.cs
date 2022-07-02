ce0f4647470dde4e3ec2f709c1266c62304ae9b3
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-07-02 12:33:09 -0700

    pass first test for Bread.cs

--- Bakery.Tests/Model.Tests/BreadTests.cs
+++ Bakery.Tests/Model.Tests/BreadTests.cs
@@ -7,6 +7,18 @@ namespace Bakery.Tests
   [TestClass]
   public class BreadTests
   {
+    [TestMethod]
+    public void SetProperties_BreadConstuctorSetsProperties_IntDouble()
+    {
+      int orderQuantity = 1;
+      double cost = 5;
+
+      Bread newBread = new Bread(orderQuantity, cost);
+      int totalUserOrder = newBread.OrderQuantity;
+      double totalCost = newBread.Cost;
+      Assert.AreEqual(totalUserOrder, newBread.OrderQuantity);
+      Assert.AreEqual(totalCost, newBread.Cost);
+    }
     
   }
 }
