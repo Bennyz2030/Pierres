d9581559e45167157625a3c7ba3379af4b6eebb3
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-07-02 18:14:26 -0700

    add questions lines to TopMenu and BreadOrder

--- Program.cs
+++ Program.cs
@@ -17,6 +17,26 @@ namespace Bakery
     {
       Console.WriteLine("Welcome to Pierre's Bakery!");
       Console.WriteLine("Would you like to order some bread? Type Y for yes, or N for no.");
+
+      string menuChoice = Console.ReadLine().ToUpper();
+      bread = new Bread (0,0);
+      pastry = new Pastry (0,0);
+       if (menuChoice == "Y")
+       {
+         breadOrder();
+       }
     }
+
+    public class BreadOrder()
+    {
+     public void BreadOrder()
+    {
+      Console.WriteLine("Bread is $5 a loaf. For every 2 loaves you buy, get a third for free!");
+      Console.WriteLine("How many loaves would you like?");
+
+      int orderQuantity = 0;
+      string userInput = Console.ReadLine();
+    }
+    TopMenu();
   }
 }
